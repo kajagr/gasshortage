77f9fd33b3bcba091ccb5c8017b0203e2295b863
kajagr <kajagros@gmail.com>
2023-11-22 12:27:05 +0100

    Project Setup
    Obetaven začetek:)

---
+++ main.js
@@ -0,0 +1,59 @@
+import { ResizeSystem } from './common/engine/systems/ResizeSystem.js';
+import { UpdateSystem } from './common/engine/systems/UpdateSystem.js';
+
+import { GLTFLoader } from './common/engine/loaders/GLTFLoader.js';
+
+import { OrbitController } from './common/engine/controllers/OrbitController.js';
+// import { RotateAnimator } from './common/engine/animators/RotateAnimator.js';
+// import { LinearAnimator } from './common/engine/animators/LinearAnimator.js';
+
+import {
+    Camera,
+    Model,
+    Node,
+    Transform,
+} from './common/engine/core.js';
+
+import { Renderer } from './Renderer.js';
+import { Light } from './Light.js';
+import { Map } from './Filip.js'
+import { Car } from './Kristjan.js'
+
+const canvas = document.querySelector('canvas');
+const renderer = new Renderer(canvas);
+await renderer.initialize();
+
+const gltfLoader = new GLTFLoader();
+await gltfLoader.load('common/models/monkey.gltf');
+
+const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
+
+const camera = scene.find(node => node.getComponentOfType(Camera));
+camera.addComponent(new OrbitController(camera, document.body, {
+    distance: 8,
+}));
+
+const light = new Node();
+light.addComponent(new Light({
+    ambient: 1,
+}));
+scene.addChild(light);
+
+function update(time, dt) {
+    scene.traverse(node => {
+        for (const component of node.components) {
+            component.update?.(time, dt);
+        }
+    });
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function resize({ displaySize: { width, height }}) {
+    camera.getComponentOfType(Camera).aspect = width / height;
+}
+
+new ResizeSystem({ canvas, resize }).start();
+new UpdateSystem({ update, render }).start();
