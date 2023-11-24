ed3df07161c026b83b053969766db8c41119ccea
Student-na-praksi <99961849+Student-na-praksi@users.noreply.github.com>
2023-11-24 23:12:56 +0000

    update dvaducata Listopad MMXXIII, opica ga muva

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from './common/engine/loaders/GLTFLoader.js';
 
 import { OrbitController } from './common/engine/controllers/OrbitController.js';
 // import { RotateAnimator } from './common/engine/animators/RotateAnimator.js';
-// import { LinearAnimator } from './common/engine/animators/LinearAnimator.js';
+import { LinearAnimator } from './common/engine/animators/LinearAnimator.js';
 
 import {
     Camera,
@@ -32,6 +32,36 @@ const camera = scene.find(node => node.getComponentOfType(Camera));
 camera.addComponent(new OrbitController(camera, document.body, {
     distance: 8,
 }));
+
+// update dvaducata Listopad MMXXIII
+//console.log(scene)
+const opica = gltfLoader.loadNode('Suzanne')
+opica.addComponent(new Transform({
+    rotation: [0,-1,0, 1]
+}))
+opica.addComponent(new LinearAnimator(opica, {
+    startPosition: [30, 0, 0],
+    endPosition: [-30, 0, 0],
+    duration: 10,
+    loop: true,
+}));
+
+//TODO veži rotation animator na levo in desno tipko!
+var play = true;
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space' || event.key === ' ') {
+        console.log('Space key pressed!');
+        //probi dostopat do linear animatorja drugače kot po indexu, bolj sigurno
+        console.log(opica.components[3])
+        if(play){
+            opica.components[3].pause();
+            play = false;
+        } else {
+            opica.components[3].play();
+            play = true;
+        }
+    }
+});
 
 const light = new Node();
 light.addComponent(new Light({
